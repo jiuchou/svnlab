d58717d19bd0adc5b05aacbde2c8fb1ac3583c59
jiuchou <315675275@qq.com>
2019-01-08 00:14:30 +0800

    初始化svn模块

--- backend/svn/urls.py
+++ backend/svn/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from backend.svn import views
 
 urlpatterns = [
+    path('getSVNPathList', views.getSVNPathList),
+    path('getSVNPathDetail', views.getSVNPathDetail),
 ]
