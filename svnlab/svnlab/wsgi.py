fca607c4903eb75ee9c1e1209a5e296a1293028d
jiuchou <45912997+jiuchou@users.noreply.github.com>
2019-01-15 19:48:19 +0800

    Update wsgi.py
    问题: 项目名称与主名称冲突，导致部署问题
    Bugfix: 修改项目根目录名称

--- svnlab/svnlab/wsgi.py
+++ svnlab/svnlab/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "svnlab.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "svnlabManage.settings")
 
 application = get_wsgi_application()
